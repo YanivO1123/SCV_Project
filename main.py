333b33a8f1ae67e0780bec6f0d0b1308384a17cb
Sterre <s.c.noorthoek@student.tudelft.nl>
2021-05-01 17:18:00 +0200

    Load yolov5

--- main.py
+++
@@ -1,9 +0,0 @@
-#import torch
-#import yolov5
-
-
-if __name__ == '__main__':
-    print(f'Hi')  # Press ⌘F8 to toggle the breakpoint.
-    # model
-    # model = yolov5.load('yolov5s')
-    #model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
