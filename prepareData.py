33c5c7a2426ade05faf0f6310282f8f3a103312c
Sterre <s.c.noorthoek@student.tudelft.nl>
2021-05-17 10:24:58 +0200

    Organised dataset + create txt files for bounding boxes

--- prepareData.py
+++ prepareData.py
@@ -5,17 +5,23 @@ import pandas as pd
 # c3 = upper right corner
 # c4 = lower right corner
 
-data = pd.read_csv("Plant_Phenotyping_Datasets/Plant_Phenotyping_Datasets/Plant/Ara2012/ara2012_plant001_bbox.csv")
+list_ara2012 = ["%03d" % i for i in range(1, 121)]
+list_ara2013_canon = ["%03d" % i for i in range(1, 166)]
 
-data.columns =['index', 'c1x', 'c1y', 'c2x', 'c2y', 'c3x', 'c3y', 'c4x', 'c4y']
 
-f= open("testPrepareData.txt","w+")
+for i in list_ara2013_canon:
+    print(i)
 
-for i in range(len(data.index)):
-    x_coordinate = data.iloc[i]['c2x']
-    y_coordinate = data.iloc[i]['c2y']
-    width = data.iloc[i]['c3x'] - data.iloc[i]['c2x']
-    height = data.iloc[i]['c2y'] - data.iloc[i]['c1y']
-    dataForYolo = [x_coordinate, y_coordinate, width, height]
-    f.write("%d %d %d %d\n" % (dataForYolo[0], dataForYolo[1], dataForYolo[2], dataForYolo[3]))
+    data = pd.read_csv("Plant_Phenotyping_Datasets/Ara2013-Canon/ara2013_plant" + i + "_bbox.csv")
+    data.columns =['index', 'c1x', 'c1y', 'c2x', 'c2y', 'c3x', 'c3y', 'c4x', 'c4y']
 
+    f= open("imgs/Ara2013-Canon/ara2013_plant" + i + "_bbox.txt","w+")
+
+    for i in range(len(data.index)):
+        x_coordinate = data.iloc[i]['c2x']
+        y_coordinate = data.iloc[i]['c2y']
+        width = data.iloc[i]['c3x'] - data.iloc[i]['c2x']
+        height = data.iloc[i]['c2y'] - data.iloc[i]['c1y']
+        dataForYolo = [x_coordinate, y_coordinate, width, height]
+        f.write("%d %d %d %d\n" % (dataForYolo[0], dataForYolo[1], dataForYolo[2], dataForYolo[3]))
+    f.close()
