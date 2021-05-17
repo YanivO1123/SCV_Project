4902eb668bbd0c53a7cd96a02af8c8a6a7d2894b
Sterre <s.c.noorthoek@student.tudelft.nl>
2021-05-17 14:28:08 +0200

    Added label and images files

--- prepareData.py
+++ prepareData.py
@@ -9,13 +9,13 @@ list_ara2012 = ["%03d" % i for i in range(1, 121)]
 list_ara2013_canon = ["%03d" % i for i in range(1, 166)]
 
 
-for i in list_ara2013_canon:
+for i in list_ara2012:
     print(i)
 
-    data = pd.read_csv("Plant_Phenotyping_Datasets/Ara2013-Canon/ara2013_plant" + i + "_bbox.csv")
+    data = pd.read_csv("original_dataset/Ara2012/ara2012_plant" + i + "_bbox.csv")
     data.columns =['index', 'c1x', 'c1y', 'c2x', 'c2y', 'c3x', 'c3y', 'c4x', 'c4y']
 
-    f= open("imgs/Ara2013-Canon/ara2013_plant" + i + "_bbox.txt","w+")
+    f= open("labels/Ara2012/ara2012_plant" + i + ".txt","w+")
 
     for i in range(len(data.index)):
         x_coordinate = data.iloc[i]['c2x']
@@ -23,5 +23,5 @@ for i in list_ara2013_canon:
         width = data.iloc[i]['c3x'] - data.iloc[i]['c2x']
         height = data.iloc[i]['c2y'] - data.iloc[i]['c1y']
         dataForYolo = [x_coordinate, y_coordinate, width, height]
-        f.write("%d %d %d %d\n" % (dataForYolo[0], dataForYolo[1], dataForYolo[2], dataForYolo[3]))
+        f.write("%d %d %d %d %d\n" % (1, dataForYolo[0], dataForYolo[1], dataForYolo[2], dataForYolo[3]))
     f.close()
