0a12275f121eddbcac17cd39d1aebd2b61cc1a35
YanivOren <Y.Oren@student.tudelft.nl>
2021-05-22 11:36:22 +0300

    further fixes to data gen script and the gened labels

--- plotBoundingBoxes.py
+++ plotBoundingBoxes.py
@@ -15,16 +15,16 @@ for i in ["001"]:#list_ara2012:
     height = image.shape[0]
     width = image.shape[1]
     colors = [[0, 0, 255], [50, 68, 168], [50, 168, 58], [168, 164, 50], [224, 115, 20], [252, 3, 136], [3, 252, 235], [0, 0, 255], [50, 68, 168], [50, 168, 58], [168, 164, 50], [224, 115, 20], [252, 3, 136], [3, 252, 235], [0, 0, 255], [50, 68, 168], [50, 168, 58], [168, 164, 50], [224, 115, 20], [252, 3, 136], [3, 252, 235], [0, 0, 255], [50, 68, 168], [50, 168, 58], [168, 164, 50], [224, 115, 20], [252, 3, 136], [3, 252, 235]]
-    for i in range(len(coordinates.index)):
-        x_coordinate_left = round(coordinates.iloc[i]['x'] * width)
-        x_coordinate_right = round((coordinates.iloc[i]['x'] + coordinates.iloc[i]['w']) * width)
-        y_coordinate_top = round(coordinates.iloc[i]['y'] * height)
-        y_coordinate_bottom = round((coordinates.iloc[i]['y'] + coordinates.iloc[i]['h']) * height)
+    for j in range(len(coordinates.index)):
+        x_coordinate_left = round(coordinates.iloc[j]['x'] * width)
+        x_coordinate_right = round((coordinates.iloc[j]['x'] + coordinates.iloc[j]['w']) * width)
+        y_coordinate_top = round(coordinates.iloc[j]['y'] * height)
+        y_coordinate_bottom = round((coordinates.iloc[j]['y'] + coordinates.iloc[j]['h']) * height)
 
-        image = cv2.circle(image, (x_coordinate_left, y_coordinate_top), radius=3, color=(colors[i][0], colors[i][1], colors[i][2]), thickness=-1)
-        image = cv2.circle(image, (x_coordinate_right, y_coordinate_top), radius=3, color=(colors[i][0], colors[i][1], colors[i][2]), thickness=-1)
-        image = cv2.circle(image, (x_coordinate_left, y_coordinate_bottom), radius=3, color=(colors[i][0], colors[i][1], colors[i][2]), thickness=-1)
-        image = cv2.circle(image, (x_coordinate_right, y_coordinate_bottom), radius=3, color=(colors[i][0], colors[i][1], colors[i][2]), thickness=-1)
+        image = cv2.circle(image, (x_coordinate_left, y_coordinate_top), radius=3, color=(colors[j][0], colors[j][1], colors[j][2]), thickness=-1)
+        image = cv2.circle(image, (x_coordinate_right, y_coordinate_top), radius=3, color=(colors[j][0], colors[j][1], colors[j][2]), thickness=-1)
+        image = cv2.circle(image, (x_coordinate_left, y_coordinate_bottom), radius=3, color=(colors[j][0], colors[j][1], colors[j][2]), thickness=-1)
+        image = cv2.circle(image, (x_coordinate_right, y_coordinate_bottom), radius=3, color=(colors[j][0], colors[j][1], colors[j][2]), thickness=-1)
 
     #cv2.imwrite("images/Ara2012_with_bbox/ara2012_plant" + i + "_bbox.png", image)
 
